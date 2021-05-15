8e7a2e6d9e9cd4787dfb5b8a93e0f55ea1ac54e9
Kristofer Kangro <kkangro7@gmail.com>
2021-05-15 18:15:36 +0300

    first commit

---
+++ palindrome.js
@@ -0,0 +1,25 @@
+function onlyAlphanumeric(str) {
+  let regEx = /[^\W_]+/g;
+  let checkStr = str.match(regEx).join("").toLowerCase();
+  return checkStr;
+}
+
+function reverseStr(s) {
+  let splitString = s.split("");
+  return splitString.reverse().join("");
+}
+
+function palindrome(aStr) {
+  aStr = onlyAlphanumeric(aStr);
+  let reversedAStr = reverseStr(aStr);
+
+  if (aStr === reversedAStr) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+}
+
+console.log(palindrome("five|\_/|four"))
