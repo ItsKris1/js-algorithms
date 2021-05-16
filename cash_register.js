24e13edeab75559a3838b212dce55d2848ad2d8b
Kristofer Kangro <kkangro7@gmail.com>
2021-05-16 20:55:13 +0300

    cash register

--- cash_register.js
+++ cash_register.js
@@ -1,20 +1,35 @@
 function checkCashRegister(price, cash, cid) {
+
+    function getCidSum(arr) {
+        return arr.reduce(function(total, val){
+        total += val[1];
+        return total;
+    }, 0)
+}
+
     cid = cid.reverse()
 
     const currencies = [
-        ["One-hundred dollars", 100],
-        ["Twenty dollars", 20],
-        ["Ten dollars", 10],
-        ["Five dollars", 5],
-        ["Dollar", 1],
-        ["Quarter", 0.25],
-        ["Dime", 0.1],
-        ["Nickel", 0.05],
-        ["Penny", 0.01],
+        ["ONE HUNDRED", 100],
+        ["TWENTY", 20],
+        ["TEN", 10],
+        ["FIVE", 5],
+        ["ONE", 1],
+        ["QUARTER", 0.25],
+        ["DIME", 0.1],
+        ["NICKEL", 0.05],
+        ["PENNY", 0.01],
     ]
 
-    let payback = [];
+    
     let change = cash - price;
+    let cidSum = getCidSum(cid)
+
+    if (cidSum < change) {
+        return {status: 'INSUFFICIENT_FUNDS', change: []}
+    }
+
+    let payback = [];
 
     for (let i = 0; i < currencies.length; i++) {
         if (change >= currencies[i][1]) {
@@ -35,20 +50,35 @@ function checkCashRegister(price, cash, cid) {
             change -= unitVal;
             change = Math.round(change * 100) / 100; // Round to 2 decimal places
 
-            payback.push([unit, unitVal])
+            if (payback.includes(unit)) {
+                let idx = payback.indexOf(unit);
+                payback[idx + 1] += unitVal; 
+    
+            } else {
+                payback.push(unit, unitVal);
+            }
+
+            i--;
 
             if (change === 0) {
-                return payback;
+                
+                change = [];
 
-            } else {
-                i--;
-            }
+                for (let i = 0; i < payback.length; i+=2) {
+                    change.push(payback.slice(i, i+2));
+                }
+                console.log(getCidSum(cid))
+                if (getCidSum(cid) === 0) {
+                    return {status: "CLOSED", change: change};
+                } else {
+                    return {status: "OPEN", change: change};
+                }
+                
         }
+        
     }
+}
 }
 
 
-console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-/*
-QUARTER - 4.25
-*/
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
--- testing.js
+++ testing.js
@@ -1,3 +1,3 @@
-let l = [5, 3, 2];
+let anArray = [[2, 3], [3, 5]];
 
-console.log(l.reverse())
+console.log(5 in anArray);
